4486f1932516c1dd5ebc0e84a829e88f45502f73
xuyi123454321 <turambar@mail.ustc.edu.cn>
2015-11-28 19:50:12 +0800

    新文件：   access_token.log 	新文件：   access_token.py

---
+++ access_token.py
@@ -0,0 +1,37 @@
+from urllib import request
+import time
+import redis
+import json
+
+class TokenAccessError(Exception):
+    def __str__(self):
+        return "Cannot access token"
+
+def access_token(appid, appsecret):
+    res = request.urlopen("https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=%s&secret=%s"%(appid, appsecret))
+    res_dict = json.loads(res.read().decode())
+
+    if not 'access_token' in res_dict.keys():
+        raise TokenAccessError
+        return (0, 0)
+    else:
+        return (res_dict['access_token'], res_dict['expires_in'])
+
+def main():
+    log_file = open("access_token.log", "a")
+    app_data = json.load(open("app_data.json", "r"))
+
+    token_cache = redis.Redis(host="localhost", port=6379, db=0)
+
+    while True:
+        try:
+            token, expires_in = access_token(app_data['appID'],
+                                        app_data['appsecret'])
+            token_cache['token'] = token
+            time.sleep(expires_in)
+        except Exception as e:
+            log_file.write("%s:%s"%(time.ctime(), e))
+            time.sleep(60)
+        
+if __name__=='__main__':
+    main()
