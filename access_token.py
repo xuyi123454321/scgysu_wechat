28a80549fb6f5e40be64c75140030d0698c794d7
xuyi123454321 <turambar@mail.ustc.edu.cn>
2015-11-28 20:00:04 +0800

    修改：     access_token.py

--- access_token.py
+++ access_token.py
@@ -1,9 +1,19 @@
+#!/usr/bin/env python
+'''
+access token from https://api.weixin.qq.com/cgi-bin/token using method GET.
+token is stored in redis.
+to get token call redis.Redis(host='localhost', port=6379, db=0) to get the database.
+'''
+
 from urllib import request
 import time
 import redis
 import json
 
 class TokenAccessError(Exception):
+    '''
+    Exception raised when only responce from weixin goes wrong
+    '''
     def __str__(self):
         return "Cannot access token"
 
