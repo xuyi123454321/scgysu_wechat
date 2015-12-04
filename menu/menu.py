946d28b4e4cbf32da07855f89425a332f1d8cd20
xuyi123454321 <turambar@mail.ustc.edu.cn>
2015-12-04 22:18:58 +0800

    修改：     __pycache__/models.cpython-35.pyc 	修改：     menu.json 	修改：     menu.py 	修改：     models.py 	修改：     ../message_receive/urls.py 	修改：     ../scgysu_wechat/__pycache__/settings.cpython-35.pyc 	修改：     ../scgysu_wechat/settings.py 	migrations/0002_auto_20151204_1908.py 	migrations/__pycache__/0002_auto_20151204_1908.cpython-35.pyc

---
+++ menu/menu.py
@@ -0,0 +1,25 @@
+from urllib import request
+import json
+import redis
+import time
+
+url = "https://api.weixin.qq.com/cgi-bin/menu/create?access_token=%s"
+
+def create_menu():
+    log_file = open("menu.log", "a")
+
+    menu_file = open("menu.json", "r") #using json for now, chang to db later
+    menu_data = json.dumps(menu_file.read())
+    
+    token_db = redis.Redis(host="localhost", port=6379, db=0)
+    token = token_db.get("token")
+
+    print(menu_data.encode("utf-8"))
+    res = request.urlopen(url%token.decode(), menu_data.encode("utf-8"))
+    log_file.write("%s %s\n"%(time.ctime(), res.read().decode()))
+
+    log_file.close()
+    menu_file.close()
+
+if __name__=='__main__':
+    create_menu()
